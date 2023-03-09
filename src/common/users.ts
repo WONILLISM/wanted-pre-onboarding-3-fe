0c03fb8c815c45e6e33076ed4be2a70773399d44
WONILLISM <wonillism@gmail.com>
2023-03-09 19:47:53 +0900

    day02

--- src/common/users.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../interfaces/User";
-
-export const users: User[] = [
-  { username: "user1", password: "1234", userInfo: { name: "name1" } },
-  { username: "user2", password: "1234", userInfo: { name: "name2" } },
-  { username: "user3", password: "1234", userInfo: { name: "name3" } },
-  { username: "user4", password: "1234", userInfo: { name: "name4" } },
-];
